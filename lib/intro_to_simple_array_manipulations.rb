1d7f6fe77d9e535f22c299a7e767d5a041e7fcb2
Derric Fragosa <fderric@gmail.com>
2019-09-14 06:57:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,38 +1,30 @@
 def using_push(array, element)
-  colors_in_the_rainbow = ["red","orange","yellow","green","blue","indigo"] 
-    next_color = "violet"
-    colors_in_the_rainbow.push(next_color)
-  end
+      array.push(element)
+end
 
 def using_unshift(array, element)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift("Staten Island")
+     array.unshift(element)
 end
 
 def using_pop(array)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"] 
-  continents.pop 
+     array.pop 
 end
 
 def pop_with_args(array)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"] 
-  dog_breeds.pop(2)
+      array.pop(2)
   end 
   
 def using_shift (array)
-  favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-   favorite_cities.shift
+     array.shift
  end 
  
  def shift_with_args(array)
-   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-   ice_cream_brands.shift(2) 
+      array.shift(2) 
  end 
  
  def using_concat(array,array)
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports, cars", "flatiron school"]
-    my_favorite_things.concat(more_favs)
+
+    array.concat(array)
  end
  
  def using_insert(array,element)
